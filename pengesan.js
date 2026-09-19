98104bad52f61ce3e38f78c4da5d3450688ac846
marlinaazmah-mell <marlina.azmah@gmail.com>
2026-09-19 10:45:27 +0800

    Update pengesan.js

--- pengesan.js
+++ pengesan.js
@@ -1,1 +1,21 @@
+function kesanGarisan() {
 
+    const gambar = document.getElementById("previewPola");
+    const status = document.getElementById("statusKamera");
+
+    if (!gambar.src) {
+        status.innerHTML = "⚠️ Muat naik gambar pola dahulu.";
+        return;
+    }
+
+    status.innerHTML = "🔎 Sedang menganalisis pola...";
+
+    setTimeout(function() {
+
+        status.innerHTML = `
+            🟢 <strong>POLA BERJAYA DIKESAN</strong><br><br>
+            Sistem dapat membaca imej pola.
+        `;
+
+    }, 1000);
+}
